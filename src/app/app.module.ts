78befa9b0e1c8f67256b00530164cc84a3dc78c2
Lucas Sanches <sanches.lz@hotmail.com>
2020-10-04 11:26:25 -0300

    Creating tictactoe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TictactoeComponent } from './core/tictactoe/tictactoe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TictactoeComponent
   ],
   imports: [
     BrowserModule,
